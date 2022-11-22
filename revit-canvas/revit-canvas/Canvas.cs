71a8f5019153bbb9743ce4dfcc0bcd01765a8f2c
Denise Booghean <denis.boghean@leviatan.ro>
2022-11-22 12:53:51 +0200

    Creare incapere din formularul de creare.Adaugare posibilitate de incapere neregulata

---
+++ revit-canvas/revit-canvas/Canvas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace revit_canvas
+{
+    public partial class Canvas : UserControl
+    {
+        public Canvas()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            SetStyle(ControlStyles.UserPaint, true);
+        }
+    }
+}
